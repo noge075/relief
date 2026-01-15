294b287f60ea20ca62d57077aca4309a6e554172
Zsolt Janes <zsolt.janes@gmail.com>
2026-01-15 14:34:39 +0100

    Fortify installed, basic layout added, login screen added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'dashboard')
+    ->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+require __DIR__.'/settings.php';
